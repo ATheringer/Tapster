69be5866d7e3b8618314cfdbff21edb8968f8854
Kyle <kliberti@bu.edu>
2015-04-17 03:12:21 +0000

    File to be used for changing password

---
+++ forgotpass.php
@@ -0,0 +1,12 @@
+<html>
+	<style>
+		body {
+                      	background-color: #d3d3d3;
+                        font-family: "Ubuntu", sans-serif;
+                        color: purple;
+                }
+	</style>
+
+	<form action="checkforgot.php" method=post>
+
+</html>
